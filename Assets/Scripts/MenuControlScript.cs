d86ac1660c1ce30d8798607a668b36aa248776e8
hoang phuc <dr.hoangphuc@hotmail.com>
2016-02-17 21:16:43 +0700

    add rooms screen and store screen

---
+++ Assets/Scripts/MenuControlScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuControlScript : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+            if (hit.collider == null) return;
+            if (hit.collider.gameObject.tag == "menu_lobbybutton")
+            {
+                Debug.Log("lo");
+            }
+            else if (hit.collider.gameObject.tag == "menu_storebutton")
+            {
+                Debug.Log("store");
+            }
+            else if (hit.collider.gameObject.tag == "menu_hightscorebutton")
+            {
+                Debug.Log("hight");
+            }
+        }
+    }
+}
